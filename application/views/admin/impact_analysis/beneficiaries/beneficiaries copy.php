d073f22ae18852954fb457a1e11b8893d5fb53b5
Navid Aziz <navidaziz98@gmail.com>
2025-06-04 08:25:11 +0500

    some changes

--- application/views/admin/impact_analysis/beneficiaries/beneficiaries copy.php
+++ application/views/admin/impact_analysis/beneficiaries/beneficiaries copy.php
@@ -1,92 +1,54 @@
-<h4></h4>
-
-
-<div class="row">
-    <div class="col-md-6">
-        <h4>Impact Analysis: Beneficiaries</h4>
-        <small>Analysis of total <strong><?php
-                                            $query = "SELECT COUNT(*) as total FROM `impact_surveys`";
-                                            echo number_format($this->db->query($query)->row()->total);
-
-                                            ?></strong> Impact Surveys</small>
-    </div>
-    <div class="col-md-6" style="text-align: right;">
-        <a target="new" href="<?php echo base_url('admin/impact_analysis/export_data/Cropping_Pattern'); ?>" class="btn btn-success btn-sm"><i class="fa fa-download" aria-hidden="true"></i> Raw Data</a>
-        <button class="btn btn-danger btn-sm" onclick="exportMultipleTablesToExcel('Irrigated_CCA',['table_1', 'table_2', 'wheat', 'maize' , 'maize_hybrid' , 'sugarcane' , 'fodder' , 'vegetable' , 'fruit_orchard' ], ['Summary', 'Crop & Component Wise' , 'wheat', 'maize' , 'maize_hybrid' , 'sugarcane' , 'fodder' , 'vegetable' , 'fruit_orchard'])"><i class="fa fa-download" aria-hidden="true"></i> Export Data in Excel</button>
-    </div>
-</div>
+<h4>Beneficiaries</h4>
 <hr />
 <div class="row">
     <div class="col-md-4">
-        <div class="card shadow-sm">
-            <div class="card-header bg-primary text-white">
-                <strong>WUA AVG. Members <small>Per Scheme</small></strong>
-            </div>
-            <div class="card-body p-0">
-                <div class="table-responsive">
 
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr style="display: none;">
-                                <th colspan="2">WUA AVG. Members <small>Per Scheme</small></th>
-                            </tr>
-                            <tr>
-                                <th>Description</th>
-                                <th>Value</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $query = "
-                                SELECT 
-                                    ROUND(AVG(wua_members), 2) AS wua_members,
-                                    ROUND(AVG(male_members), 2) AS male_members,
-                                    ROUND(AVG(female_members), 2) AS female_members,
-                                    ROUND(AVG(male_members) / (AVG(male_members) + AVG(female_members)) * 100, 2) AS male_percentage,
-                                    ROUND(AVG(female_members) / (AVG(male_members) + AVG(female_members)) * 100, 2) AS female_percentage
-                                FROM impact_surveys AS s;
-                            ";
-                            $wua = $this->db->query($query)->row();
-
-                            // Store values for Highcharts
-                            $wua_members[] = $wua->wua_members;
-                            $male_members[] = $wua->male_members;
-                            $female_members[] = $wua->female_members;
-                            ?>
-                            <tr>
-                                <td>Average Members</td>
-                                <td><?php echo $wua->wua_members; ?></td>
-                            </tr>
-                            <tr>
-                                <td>Male Members (Avg.)</td>
-                                <td><?php echo $wua->male_members; ?></td>
-                            </tr>
-                            <tr>
-                                <td>Female Members (Avg.)</td>
-                                <td><?php echo $wua->female_members; ?></td>
-                            </tr>
-                            <tr>
-                                <td>Male Members (%)</td>
-                                <td><?php echo round($wua->male_percentage, 2) . "%"; ?></td>
-                            </tr>
-                            <tr>
-                                <td>Female Members (%)</td>
-                                <td><?php echo round($wua->female_percentage, 2) . "%"; ?></td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <table class="table table-bordered table_medium">
+            <thead>
+                <tr>
+                    <th colspan="6">WUA Members</th>
+                </tr>
+                <tr>
+                    <th colspan="4">WUA Members</th>
+                </tr>
+                <tr>
+                    <th>Total <small>Avg.</small></th>
+                    <th>Male <small>Avg.</small></th>
+                    <th>Female <small>Avg.</small></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query =
+                    "SELECT SUM(wua_members) as wua_members,
+                    SUM(male_members) as male_members,
+                    SUM(female_members) as female_members
+                FROM `impact_surveys` as s";
+                $wua = $this->db->query($query)->row();
 
 
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card shadow-sm">
-            <div class="card-body">
-                <div id="wua_members_chart" style="width: 100%;  height: 330px; margin: 0 auto;"></div>
-            </div>
-        </div>
+                // Store values for Highcharts
+                $wua_members[] = $wua->wua_members;
+                $male_members[] = $wua->male_members;
+                $female_members[] = $wua->female_members;
+                ?>
+                <tr>
+                    <td><?php echo $wua->wua_members; ?></td>
+                    <td><?php echo $wua->male_members; ?></td>
+                    <td><?php echo $wua->female_members; ?></td>
+                </tr>
+                <tr>
+                    <td>Percentage</td>
+                    <td><?php echo round(($wua->male_members * 100) / $wua->wua_members, 2) . "%"; ?></td>
+                    <td><?php echo round(($wua->female_members * 100) / $wua->wua_members, 2) . "%"; ?></td>
+                </tr>
+
+            </tbody>
+
+
+        </table>
+
+        <div id="wua_members_chart"></div>
         <script>
             Highcharts.chart('wua_members_chart', {
                 chart: {
@@ -96,15 +58,15 @@
                     text: 'WUA Members'
                 },
                 xAxis: {
-                    categories: 'WUA Average Members',
+                    categories: 'WUA Members',
                     title: {
-                        text: 'Average Members'
+                        text: 'Components'
                     }
                 },
                 yAxis: {
                     min: 0,
                     title: {
-                        text: '(Avg.)'
+                        text: 'WP Kg/m<sup>3</sup> (Avg.)'
                     }
                 },
                 tooltip: {
@@ -113,92 +75,32 @@
                 },
                 plotOptions: {
                     column: {
-                        shadow: false,
-                        borderWidth: 0,
-                        dataLabels: {
-                            enabled: true,
-                            //format: '{y:,.000f}',
-                            crop: false, // Don't hide labels outside the plot area
-                            overflow: 'none', // Prevent hiding when overflowing
-                            allowOverlap: true,
-                            //rotation: -90,
-                            style: {
-                                fontSize: '9px' // Change to your desired font size
-
-                            }
-
-                        }
+                        pointPadding: 0.2,
+                        borderWidth: 0
                     }
                 },
                 series: [{
-                        name: 'AVG. Members',
+                        name: 'WUA Members',
                         data: <?php echo json_encode($wua_members, JSON_NUMERIC_CHECK); ?>
                     },
                     {
-                        name: 'Males',
-                        color: '#00A2D6',
+                        name: 'Male Members',
                         data: <?php echo json_encode($male_members, JSON_NUMERIC_CHECK); ?>
                     },
                     {
-                        name: 'Females',
-                        color: '#EF3DBB',
+                        name: 'Female Members',
                         data: <?php echo json_encode($female_members, JSON_NUMERIC_CHECK); ?>
                     }
                 ]
             });
         </script>
-    </div>
-    <div class="col-md-4">
-        <div class="card shadow-sm">
-            <div class="card-body">
-                <div id="members-pie" style="width: 100%;  height: 330px; margin: 0 auto;"></div>
-            </div>
-        </div>
-        <script type="text/javascript">
-            Highcharts.chart('members-pie', {
-                chart: {
-                    type: 'pie'
-                },
-                title: {
-                    text: 'WUA Member Gender Distribution'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
-                },
-                accessibility: {
-                    point: {
-                        valueSuffix: '%'
-                    }
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.percentage:.2f} %'
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Members',
-                    colorByPoint: true,
-                    data: [{
-                        name: 'Male',
-                        color: '#00A2D6',
-                        y: <?php echo $wua->male_percentage ?? 0; ?>
-                    }, {
-                        name: 'Female',
-                        color: '#EF3DBB',
-                        y: <?php echo $wua->female_percentage ?? 0; ?>
-                    }]
-                }]
-            });
-        </script>
+
+
+
+
 
     </div>
-</div>
-<div class="row">
+
     <div class="col-md-4">
 
         <?php
