13891699f293b3cf0a6505d7515c908b818def61
Navid Aziz <navidaziz98@gmail.com>
2025-07-04 22:24:43 +0500

    some changes

--- application/views/admin/impact_analysis/cropping_intensity/cropping_intensity.php
+++ application/views/admin/impact_analysis/cropping_intensity/cropping_intensity.php
@@ -54,7 +54,7 @@ $components = $components_result->result();
                             </tr>
                             <tr>
                                 <th>Regions</th>
-                                <th class="text-center"><small>Total</small></th>
+                                <!-- <th class="text-center"><small>Total</small></th> -->
                                 <th class="text-center">Before <small>Avg</small></th>
                                 <th class="text-center">After <small>Avg</small></th>
                                 <th class="text-center">Increase <small>Avg</small></th>
@@ -90,7 +90,7 @@ $components = $components_result->result();
                             ?>
                                 <tr>
                                     <td><?php echo ucfirst($region->region) ?></td>
-                                    <td class="text-center"><small><?php echo $row->total; ?></small></td>
+                                    <!-- <td class="text-center"><small><?php echo $row->total; ?></small></td> -->
                                     <td class="text-center"><?php echo number_format($row->before, 2); ?></td>
                                     <td class="text-center"><?php echo number_format($row->after, 2); ?></td>
                                     <td class="text-center"><?php echo number_format($increase, 2); ?></td>
@@ -101,7 +101,7 @@ $components = $components_result->result();
                         <tfoot class="font-weight-bold">
                             <tr>
                                 <th><small>Weighted Average</small></th>
-                                <td class="text-center"><small><?php echo $total; ?></small></td>
+                                <!-- <td class="text-center"><small><?php echo $total; ?></small></td> -->
                                 <td class="text-center"><?php echo number_format(round($befor_weight / $total, 2), 2); ?></td>
                                 <td class="text-center"><?php echo number_format(round($after_weight / $total, 2), 2); ?></td>
                                 <td class="text-center"><?php echo number_format(round($increase_weight / $total, 2), 2); ?></td>
@@ -295,19 +295,19 @@ $components = $components_result->result();
                             <tr>
                                 <th rowspan="2" class="align-middle">Regions</th>
                                 <?php foreach ($components as $component) { ?>
-                                    <th colspan="5" class="text-center">Component <?php echo $component->component; ?></th>
+                                    <th colspan="4" class="text-center">Component <?php echo $component->component; ?></th>
                                 <?php } ?>
-                                <th colspan="5" class="text-center">Overall Average</th>
+                                <th colspan="4" class="text-center">Overall Average</th>
                             </tr>
                             <tr>
                                 <?php foreach ($components as $component) { ?>
-                                    <th class="text-center"><small>Total</small></th>
+                                    <!-- <th class="text-center"><small>Total</small></th> -->
                                     <th class="text-center">Before <small>Avg</small></th>
                                     <th class="text-center">After <small>Avg</small></th>
                                     <th class="text-center">Increase <small>Avg</small></th>
                                     <th class="text-center">Increase <small>(%)</small></th>
                                 <?php } ?>
-                                <th class="text-center"><small>Total</small></th>
+                                <!-- <th class="text-center"><small>Total</small></th> -->
                                 <th class="text-center">Before <small>Avg</small></th>
                                 <th class="text-center">After <small>Avg</small></th>
                                 <th class="text-center">Increase <small>Avg</small></th>
@@ -371,7 +371,7 @@ $components = $components_result->result();
                                         $chart_data[$component->component]['per_increase'][] = isset($row->per_increase) ? $row->per_increase : 0;
 
                                     ?>
-                                        <td class="text-center"><small><?php echo $row->total; ?></small></td>
+                                        <!-- <td class="text-center"><small><?php echo $row->total; ?></small></td> -->
                                         <td class="text-center"><?php echo number_format($row->before, 2); ?></td>
                                         <td class="text-center"><?php echo number_format($row->after, 2); ?></td>
                                         <td class="text-center"><?php echo number_format($row->increase, 2); ?></td>
@@ -414,7 +414,7 @@ $components = $components_result->result();
                                     $chart_data['Over All']['increase'][] = isset($row->increase) ? $row->increase : 0;
                                     $chart_data['Over All']['per_increase'][] = isset($row->per_increase) ? $row->per_increase : 0;
                                     ?>
-                                    <td class="text-center"><small><?php echo $row->total; ?></small></td>
+                                    <!-- <td class="text-center"><small><?php echo $row->total; ?></small></td> -->
                                     <td class="text-center"><?php echo number_format($row->before, 2); ?></td>
                                     <td class="text-center"><?php echo number_format($row->after, 2); ?></td>
                                     <td class="text-center"><?php echo number_format($row->increase, 2); ?></td>
@@ -434,7 +434,7 @@ $components = $components_result->result();
                                     $chart_data[$component->component]['increase'][] = $component->total != 0 ? round($component->increase / $component->total, 2) : 0;
                                     $chart_data[$component->component]['per_increase'][] = $component->total != 0 ? round($component->per_increase / $component->total, 2) : 0;
                                 ?>
-                                    <td class="text-center"><small><?php echo $component->total; ?></small></td>
+                                    <!-- <td class="text-center"><small><?php echo $component->total; ?></small></td> -->
                                     <td class="text-center"><?php echo $component->total != 0 ? round($component->before / $component->total, 2) : '0.00'; ?></td>
                                     <td class="text-center"><?php echo $component->total != 0 ? round($component->after / $component->total, 2) : '0.00'; ?></td>
                                     <td class="text-center"><?php echo $component->total != 0 ? round($component->increase / $component->total, 2) : '0.00'; ?></td>
@@ -446,7 +446,7 @@ $components = $components_result->result();
                                 $chart_data['Over All']['increase'][] = $total != 0 ? round($increase / $total, 2) : 0;
                                 $chart_data['Over All']['per_increase'][] = $total != 0 ? round($per_increase / $total, 2) : 0;
                                 ?>
-                                <td class="text-center"><small><?php echo $total; ?></small></td>
+                                <!-- <td class="text-center"><small><?php echo $total; ?></small></td> -->
                                 <td class="text-center"><?php echo $total != 0 ? round($before / $total, 2) : '0.00'; ?></td>
                                 <td class="text-center"><?php echo $total != 0 ? round($after / $total, 2) : '0.00'; ?></td>
                                 <td class="text-center"><?php echo $total != 0 ? round($increase / $total, 2) : '0.00'; ?></td>
@@ -554,12 +554,12 @@ $components = $components_result->result();
                             <tr>
                                 <th rowspan="2" class="align-middle">Regions</th>
                                 <?php foreach ($sub_components as $sub_component) { ?>
-                                    <th colspan="5" class="text-center">Sub Component <?php echo $sub_component->sub_component; ?></th>
+                                    <th colspan="4" class="text-center">Sub Component <?php echo $sub_component->sub_component; ?></th>
                                 <?php } ?>
                             </tr>
                             <tr>
                                 <?php foreach ($sub_components as $sub_component) { ?>
-                                    <th class="text-center"><small>Total</small></th>
+                                    <!-- <th class="text-center"><small>Total</small></th> -->
                                     <th class="text-center">Before <small>Avg</small></th>
                                     <th class="text-center">After <small>Avg</small></th>
                                     <th class="text-center">Increase <small>Avg</small></th>
@@ -606,7 +606,7 @@ $components = $components_result->result();
                                         $sub_component->increase += $row->increase * $row->total;
                                         $sub_component->per_increase += $row->per_increase * $row->total;
                                     ?>
-                                        <td class="text-center"><small><?php echo $row->total; ?></small></td>
+                                        <!-- <td class="text-center"><small><?php echo $row->total; ?></small></td> -->
                                         <td class="text-center"><?php echo number_format($row->before, 2); ?></td>
                                         <td class="text-center"><?php echo number_format($row->after, 2); ?></td>
                                         <td class="text-center"><?php echo number_format($row->increase, 2); ?></td>
@@ -622,9 +622,9 @@ $components = $components_result->result();
                                 <th>Weighted Average</th>
 
                                 <?php foreach ($sub_components as $sub_component) { ?>
-                                    <td class="text-center">
+                                    <!-- <td class="text-center">
                                         <?php echo  $sub_component->total; ?>
-                                    </td>
+                                    </td> -->
                                     <td class="text-center">
                                         <?php echo $sub_component->total != 0 ? round($sub_component->before / $sub_component->total, 2) : '0.00'; ?>
                                     </td>
@@ -672,12 +672,12 @@ $components = $components_result->result();
                             <tr>
                                 <th rowspan="2" class="align-middle">Regions</th>
                                 <?php foreach ($categorys as $category) { ?>
-                                    <th colspan="5" class="text-center">Categories <?php echo $category->category; ?></th>
+                                    <th colspan="4" class="text-center">Categories <?php echo $category->category; ?></th>
                                 <?php } ?>
                             </tr>
                             <tr>
                                 <?php foreach ($categorys as $category) { ?>
-                                    <th class="text-center"><small>Total</small></th>
+                                    <!-- <th class="text-center"><small>Total</small></th> -->
                                     <th class="text-center">Before <small>Avg</small></th>
                                     <th class="text-center">After <small>Avg</small></th>
                                     <th class="text-center">Increase <small>Avg</small></th>
@@ -720,7 +720,7 @@ $components = $components_result->result();
                                         $category->increase += $row->increase * $row->total;
                                         $category->per_increase += $row->per_increase * $row->total;
                                     ?>
-                                        <td class="text-center"><small><?php echo $row->total; ?></small></td>
+                                        <!-- <td class="text-center"><small><?php echo $row->total; ?></small></td> -->
                                         <td class="text-center"><?php echo number_format($row->before, 2); ?></td>
                                         <td class="text-center"><?php echo number_format($row->after, 2); ?></td>
                                         <td class="text-center"><?php echo number_format($row->increase, 2); ?></td>
@@ -735,9 +735,9 @@ $components = $components_result->result();
                                 <th>Weighted Average</th>
 
                                 <?php foreach ($categorys as $category) { ?>
-                                    <td class="text-center">
+                                    <!-- <td class="text-center">
                                         <?php echo $category->total; ?>
-                                    </td>
+                                    </td> -->
                                     <td class="text-center">
                                         <?php echo $category->total != 0 ? round($category->before / $category->total, 2) : '0.00'; ?>
                                     </td>
